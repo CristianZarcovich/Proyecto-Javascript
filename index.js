3a503a1a05c3326ce4026c1796e41dca7f92ad06
Cristian Zarcovich <cristian.zarcovich@gmail.com>
2022-08-06 18:35:58 -0300

    primer app

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const nombre = prompt("¿Podrías indicarme tu nombre?");
-const saludo = "¡Hola," + " " + nombre + "!";
+let saludo = "¡Hola," + " " + nombre + "!";
 alert (saludo);
+
+const pais = prompt("¿Podrías indicarme tu país?");
+const respuesta = "¡" + pais + "!" + " ¡Hermoso país!";
+alert (respuesta);
+alert ("¡Muchas gracias por responder!")
