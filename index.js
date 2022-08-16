f56b74bea56ae40334d30168a2a4ac4b2dbae147
Cristian Zarcovich <cristian.zarcovich@gmail.com>
2022-08-16 19:01:39 -0300

    primer desafío complementario

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let nombre = prompt("Ingrese su nombre, por favor");
 while (nombre != "ESC") {
   alert("Usted a ingresado: " + nombre);
   nombre = prompt("Ingrese otro nombre");
+  
+  if (nombre == "ESC") {
+    alert ("¡Muchas gracias por participar!")
+  }
 }
