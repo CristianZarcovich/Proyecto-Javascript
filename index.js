77882f1622c30b5f9fdd979fed6e5c56c2d454e3
Cristian Zarcovich <cristian.zarcovich@gmail.com>
2022-08-30 17:57:46 -0300

    desafío complementario arrays

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function calcularPromedio() {
-  const primerInput = document.getElementById("primerTrimestre");
-  const segundoInput = document.getElementById("segundoTrimestre");
-  const tercerInput = document.getElementById("tercerTrimestre");
-  const promedio = (parseInt(primerInput.value) + parseInt(segundoInput.value) + parseInt(tercerInput.value)) / 3
+const notasPromedio = [];
+const nota1 = prompt("Ingrese nota del primer trimestre");
+const nota2 = prompt("Ingrese nota del segundo trimestre");
+const nota3 = prompt("Ingrese nota del tercer trimestre");
+notasPromedio.push(nota1, nota2, nota3);
+  const promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / notasPromedio.length
   let mensajePromocion = "No promociona."
 
   if (promedio >= 8){
@@ -10,3 +12,5 @@ function calcularPromedio() {
   }
   alert("El promedio es " + promedio + ". " + mensajePromocion)
 } 
+
+calcularPromedio();
