062b7f2b21b8ea4e8cbcadb5facea06ee9c1aea4
Cristian Zarcovich <cristian.zarcovich@gmail.com>
2022-09-15 14:31:50 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -1,59 +1,92 @@
-const alumnos = []
-
-function cargarAlumno() {
-  const alumno1 = {
-    notas: []
-  }
-
-  alumno1.nombre = document.querySelector("#nombre").value;
-  alumno1.apellido = document.querySelector("#apellido").value;
-  alumno1.edad = document.querySelector("#edad").value;
-  alumno1.notas.push (document.querySelector("#nota1").value);
-  alumno1.notas.push (document.querySelector("#nota2").value);
-  alumno1.notas.push (document.querySelector("#nota3").value);
-  alumno1.promedio = (parseInt(alumno1.notas[0]) + parseInt(alumno1.notas[1]) + parseInt(alumno1.notas[2])) / alumno1.notas.length
-
-  let mensaje = "Tu promedio es: " + alumno1.promedio + ". No promociona.";
-
-  if (isNaN(alumno1.promedio)) {
-    mensaje = "Por favor, ingrese todas las notas";
-  } else if (alumno1.promedio >= 8) {
-    mensaje = "Felicidades, promocionaste!" + " Tu promedio es: " + alumno1.promedio; 
-  } else if (alumno1.promdio >= 10) {
-    mensaje = "Ingrese valores correctos";
-  }
-
-  alert(mensaje);
-  
-  if (!isNaN(alumno1.promedio)) {
-    alumnos.push(alumno1)
-  }
-
-  imprimirAlumnoconMayorPromedio();
+const alumnosCache = localStorage.getItem("alumnos");
+let alumnos = [];
+if (alumnosCache) {
+  alumnos = JSON.parse(alumnosCache);
+  actualizarTabla()
 }
 
-function retornarAlumnoMayorPromedio() {
-  let alumnoMayorPromedio;
-  let mayorPromedio = 0;
-  for (i = 0; i < alumnos.length; i++) { 
-    const alumno = alumnos[i]; 
-    if (alumno.promedio >= mayorPromedio) {
-      mayorPromedio = alumno.promedio;
-      alumnoMayorPromedio = alumno;
+function actualizarTabla() {
+  const rank = document.querySelector("#rank");
+  rank.innerHTML = ""; 
+  const alumnosOrdenados = ordenarAlumnos(alumnos);
+
+  alumnosOrdenados.forEach((alumno, index) => {
+    let ficha = document.createElement("li")
+    let className = "item rounded-3";
+    index == 0 ? className += " n1" : null;  
+    index == 1 ? className += " n2" : null;
+    index == 2 ? className += " n3" : null;
+    ficha.className = className;
+    ficha.innerHTML = `Nombre: ${alumno.nombre}, ${alumno.apellido}</br>
+    Edad: ${alumno.edad}</br>
+    Promedio: ${alumno.promedio.toFixed(2)}</br>
+    `
+    rank.append(ficha);
+    })
+  }
+
+  function ordenarAlumnos (array) {
+  return [...array].sort((a, b) => {
+    if (a.promedio > b.promedio) {
+      return -1;
+    } else if (a.promedio < b.promedio) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+}
+
+class Alumno {
+  id;
+  nombre;
+  apellido;
+  edad;
+  notas;
+  promedio;
+
+  constructor(nombre, apellido, edad, notas) {
+    const hoy = new Date();
+    this.id = hoy.getTime();
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.notas = notas;
+    this.promedio = this.getPromedio();
+  }
+
+  getPromedio() {
+    let suma = 0;
+    this.notas.forEach(nota => {
+      suma = suma + parseInt(nota);
+    });
+    return suma / this.notas.length;
+  }
+
+  datosAlumnoJSON() {
+    return {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      edad: this.edad,
+      promedio: this.promedio
     }
   }
-
-  return alumnoMayorPromedio;
 }
 
+function cargarAlumno() {
+  const nombre = document.querySelector("#nombre").value;
+  const apellido = document.querySelector("#apellido").value;
+  const edad = document.querySelector("#edad").value;
+  const nota1 = (document.querySelector("#nota1").value);
+  const nota2 = (document.querySelector("#nota2").value);
+  const nota3 = (document.querySelector("#nota3").value);
+  const alumno = new Alumno(nombre, apellido, edad, [nota1, nota2, nota3]);
+  
+  if (!isNaN(alumno.promedio)) {
+    const objetoAlumno = alumno.datosAlumnoJSON();
+    alumnos.push(objetoAlumno);
+    localStorage.setItem("alumnos", JSON.stringify(alumnos));
+  }
 
-function imprimirAlumnoconMayorPromedio() {
-  const alumnoMayorPromedio = retornarAlumnoMayorPromedio();
-
-  const stringHTML = `<span>Nombre: ${alumnoMayorPromedio.apellido}, ${alumnoMayorPromedio.nombre}</span>
-  <span>Edad: ${alumnoMayorPromedio.edad}</span>
-  <span>Promedio: ${alumnoMayorPromedio.promedio}</span>
-  `
-  const contenedor = document.querySelector("#contenedor");
-  contenedor.innerHTML = stringHTML;
+  actualizarTabla();
 }
