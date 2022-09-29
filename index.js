0d98ef7146122a5d311ca6852c7692039a1fcb79
Cristian Zarcovich <cristian.zarcovich@gmail.com>
2022-09-29 17:27:53 -0300

    fetch

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function actualizarTabla() {
     index == 1 ? className += " n2" : null;
     index == 2 ? className += " n3" : null;
     ficha.className = className;
-    const {nombre, apellido, edad} = alumno
+    const {nombre, apellido, edad, email} = alumno
     ficha.innerHTML = `Nombre: ${nombre}, ${apellido}</br>
     Edad: ${edad}</br>
+    Email: ${email}</br>
     Promedio: ${alumno?.promedio.toFixed(2)}</br>
     `
     rank.append(ficha);
@@ -47,12 +48,13 @@ class Alumno {
   notas;
   promedio;
 
-  constructor(nombre, apellido, edad, notas) {
+  constructor(nombre, apellido, edad, email, notas) {
     const hoy = new Date();
     this.id = hoy.getTime();
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
+    this.email = email;
     this.notas = notas;
     this.promedio = this.getPromedio();
   }
@@ -70,19 +72,40 @@ class Alumno {
       nombre: this.nombre,
       apellido: this.apellido,
       edad: this.edad,
+      email: this.email,
       promedio: this.promedio
     }
   }
+
+  notificarPromedio() {
+    const bodyEmail = `Hola, ${this.nombre}. Te informamos que tu promedio es: ${this.promedio.toFixed(2)}. ¡Gracias por participar de nuestro ranking de alumnos!`
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '0e4d12bdd9mshce3175142db6f67p1460a3jsnc1f87ae8918c',
+        'X-RapidAPI-Host': 'rapidprod-sendgrid-v1.p.rapidapi.com'
+      },
+      body: `{"personalizations":[{"to":[{"email":"${this.email}"}],"subject":"Promedio de alumno"}],"from":{"email":"cristian.zarcovich@gmail.com"},"content":[{"type":"text/plain","value":"${bodyEmail}"}]}`
+    };
+    
+    fetch('https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  }
+  
 }
 
 function cargarAlumno() {
   const nombre = document.querySelector("#nombre").value;
   const apellido = document.querySelector("#apellido").value;
   const edad = document.querySelector("#edad").value;
+  const email = document.querySelector ("#email").value;
   const nota1 = (document.querySelector("#nota1").value);
   const nota2 = (document.querySelector("#nota2").value);
   const nota3 = (document.querySelector("#nota3").value);
-  const alumno = new Alumno(nombre, apellido, edad, [nota1, nota2, nota3]);
+  const alumno = new Alumno(nombre, apellido, edad, email, [nota1, nota2, nota3]);
   
   if (!isNaN(alumno.promedio)) {
     const objetoAlumno = alumno.datosAlumnoJSON();
@@ -91,6 +114,7 @@ function cargarAlumno() {
   }
 
   actualizarTabla();
+  alumno.notificarPromedio();
 
   if (!isNaN(alumno.promedio)) {
   Toastify({
